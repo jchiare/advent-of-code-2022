12a2dbd2d33435de0d69124eb7d084e24db98095
Jay Chiarella <jay.gch93@gmail.com>
2022-12-02 10:56:22 +0100

    add script and day two template

---
+++ scripts/new_day.py
@@ -0,0 +1,13 @@
+# /usr/bin/python3
+import os
+import sys
+
+day_number = sys.argv[1]
+folder = f"day{day_number}"
+
+os.mkdir(folder)
+
+task_one_file = open(f"{folder}/task_one.py", "w")
+task_two_file = open(f"{folder}/task_two.py", "w")
+
+
