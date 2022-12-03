454af6b8234dcb44b14bae0fcba9f4b5bde26a24
Jay Chiarella <jay.gch93@gmail.com>
2022-12-03 09:09:26 +0100

    complete day 3 task 2

---
+++ day3/task_two.py
@@ -0,0 +1,30 @@
+def get_priority(letter : str) -> int:
+    char_point = ord(letter)
+    
+    if char_point >= 97: # lower case 
+        return char_point - 96
+    return char_point - 65 + 27 # uppercase
+
+def main():
+
+    with open("data.txt", "r") as file:
+
+        matching_rucksack_characters = list()
+        file_as_list = file.readlines()
+        for i in range(0,len(file_as_list),3):
+            stripped_rucksacks = [s.strip() for s in file_as_list[i: i + 3]]
+            first_rucksack = set(stripped_rucksacks[0])
+            second_rucksack = set(stripped_rucksacks[1])
+
+            searched_chars = set()
+            for char in stripped_rucksacks[2]:
+                if char not in searched_chars:
+                    priority = get_priority(char)
+                    if char in first_rucksack and char in second_rucksack:
+                        matching_rucksack_characters.append(priority)
+                    searched_chars.add(char)
+             
+        print(sum(matching_rucksack_characters))
+
+if __name__ == "__main__":
+    main()
