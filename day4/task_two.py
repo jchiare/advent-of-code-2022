dc753edeafe2fa29ce94c5a47391e062809b4251
Jay Chiarella <jay.gch93@gmail.com>
2022-12-04 07:37:50 +0100

    format

--- day4/task_two.py
+++ day4/task_two.py
@@ -1,20 +1,30 @@
 def main():
-    
+
     with open("data.txt", "r") as file:
         count = 0
         for line in file:
-            assignment_one, assignment_two = line.split(',')
-            
-            first_number_assignment_one, second_number_assignment_one = assignment_one.split('-')
-            first_number_assignment_two, second_number_assignment_two = assignment_two.split('-')
-            
-            if int(first_number_assignment_two) >= int(second_number_assignment_two) and int(first_number_assignment_one) <= int(second_number_assignment_two):
+            assignment_one, assignment_two = line.split(",")
+
+            (
+                first_number_assignment_one,
+                second_number_assignment_one,
+            ) = assignment_one.split("-")
+            (
+                first_number_assignment_two,
+                second_number_assignment_two,
+            ) = assignment_two.split("-")
+
+            if int(second_number_assignment_one) >= int(
+                second_number_assignment_two
+            ) and int(first_number_assignment_one) <= int(first_number_assignment_two):
                 count += 1
-            elif int(second_number_assignment_two) >= int(first_number_assignment_two) and int(first_number_assignment_two) <= int(second_number_assignment_one):
+            elif int(second_number_assignment_two) >= int(
+                second_number_assignment_one
+            ) and int(first_number_assignment_two) <= int(first_number_assignment_one):
                 count += 1
-                
+
         print(count)
-            
+
 
 if __name__ == "__main__":
     main()
--- scripts/new_day.py
+++ scripts/new_day.py
@@ -8,12 +8,14 @@ folder = f"day{day_number}"
 os.mkdir(folder)
 
 BASE_TASK_PYTHON_CODE = """
-import os 
+import sys
 
 def main():
 
-    cwd = os.getcwd()
-    with open(os.path.join(cwd, "data.txt"), "r") as file:
+    use_sample_data = sys.argv[1]
+    data_file = "data.txt" if not use_sample_data else "sample-data.txt"
+
+    with open(data_file, "r") as file:
         for line in file:
             print('do something')
 
@@ -24,6 +26,8 @@ if __name__ == "__main__":
 task_one_file = open(f"{folder}/task_one.py", "w")
 task_two_file = open(f"{folder}/task_two.py", "w")
 data_file = open(f"{folder}/data.txt", "w")
+sample_data_file = open(f"{folder}/sample-data.txt", "w")
+
 
 task_one_file.write(BASE_TASK_PYTHON_CODE)
 task_two_file.write(BASE_TASK_PYTHON_CODE)
@@ -31,3 +35,4 @@ task_two_file.write(BASE_TASK_PYTHON_CODE)
 task_one_file.close()
 task_two_file.close()
 data_file.close()
+sample_data_file.close()
