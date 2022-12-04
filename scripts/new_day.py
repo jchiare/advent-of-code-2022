dcac0de0823074db0421d16b86a837fbe698c11c
Jay Chiarella <jay.gch93@gmail.com>
2022-12-04 07:53:49 +0100

    improve script

--- scripts/new_day.py
+++ scripts/new_day.py
@@ -12,7 +12,7 @@ import sys
 
 def main():
 
-    use_sample_data = sys.argv[1]
+    use_sample_data = sys.argv[1] if len(sys.argv) > 1 else None
     data_file = "data.txt" if not use_sample_data else "sample-data.txt"
 
     with open(data_file, "r") as file:
