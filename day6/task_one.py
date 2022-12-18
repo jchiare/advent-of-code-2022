bf25856798a23bba96aa6eecbbd221642f8ddc5a
Jay Chiarella <jay.gch93@gmail.com>
2022-12-18 09:08:06 -0500

    finish day6

---
+++ day6/task_one.py
@@ -0,0 +1,26 @@
+
+import sys
+
+def main():
+
+    use_sample_data = sys.argv[1] if len(sys.argv) > 1 else None
+    data_file = "data.txt" if not use_sample_data else "sample-data.txt"
+
+    with open(data_file, "r") as file:
+        last_four_chars = []
+        for line in file:
+            for i in range(0,len(line)):
+                
+                
+                last_four_chars.append(line[i])
+                if len(last_four_chars) >= 5:
+                    del last_four_chars[0]
+                
+                unique_last_four_chars = list(set(last_four_chars))
+                
+                if len(unique_last_four_chars) == 4:
+                    print('index: ', i + 1)
+                    break
+
+if __name__ == "__main__":
+    main()
